38acdab7dad57d5940e25eb09632d4e7bd4013c4
Rog319 <al18760466@ite.edu.mx>
2022-04-05 23:10:48 -0700

    se agrego formulario busqueda de ip y estilo
    Se agrego el formulario con dos campos para colocar un rango de Ip la inicial y la final, y también un botón para buscar, por ultimo se agrego algo de estilos básicos

--- CxProyecto/index.php
+++ CxProyecto/index.php
@@ -1,3 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitorizacion de IPs</title>
+
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel="preload" href="css/style.css" as="style">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body> 
+
+
+    <header class="header">
+
+        <div class="header__texto">
+            <h2 class="no-margin">Monitorizador de Ip</h2>
+        </div>
+    </header>
+
+    <form action="" class="formulario">
+        <div class="contenedor-campos">
+            <div class="campos">
+                <label for="">Direccion inicial</label>
+                <input class="input-text" type="text" placeholder="192.123.1.0">
+            </div>
+            <div class="campos">
+                <label for="">Direccion final</label>
+                <input class="input-text" type="text" placeholder="192.123.1.5">
+            </div>
+            <div class="">
+                <input class="boton " type="submit" value="Buscar">
+            </div>
+            
+        </div>
+
+        
+    </form>
+    
+</body>
+</html>
 <?php 
 
     function buscar_rango($inicio, $fin) {
@@ -22,22 +67,22 @@
 
     // Table
     echo '<font face=Lucida Console>';
-    echo "<table border=1 style=border-collapse:collapse>
+    echo "<table class='contenedor table' border=1 style=border-collapse:collapse>
     <th colspan=4> Ping Monitoring </th>
     <tr>
-        <td width=20></td>
-        <td width=130>IP</td>
-        <td width=110>Estado</td>
+        <td width=30></td>
+        <td width=250>IP</td>
+        <td width=220>Estado</td>
     </tr>
     ";
     foreach($results as $item => $k){
         echo "<tr>";
-        echo "<td>".$item."</td>";
-        echo "<td>".$iplista[$item]."</td>";
+        echo "<td class='text-decoration'>".$item."</td>";
+        echo "<td class='text-decoration'>".$iplista[$item]."</td>";
         if($results[$item]==0){
-            echo "<td style=color:green>Conectado</td>";
+            echo "<td class='text-decoration' style=color:green>Conectado</td>";
         } else {
-            echo "<td style=color:grey>Desconectado</td>";
+            echo "<td class='text-decoration' style=color:grey>Desconectado</td>";
         }
         echo "</tr>";
     }
@@ -53,15 +98,3 @@
 
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>CX Proyecto</title>
-    </head>
-
-    <body>
-        <h1>Proyecto Qlero</h1>
-        <p>De un profe qlero</p>
-    </body>
-
-</html>
