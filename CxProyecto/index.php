095e8e56e41aaa228957481344da599ab7462170
Alejandro-Lopez-V <al18760444@ite.edu.mx>
2022-05-17 18:08:41 -0700

    PIN

--- CxProyecto/index.php
+++ CxProyecto/index.php
@@ -12,6 +12,9 @@
     $buscar = false;
     $actualizar = false;
     $eliminar = false;
+    $pinCorrecto = false;
+    $clave = "1111";
+    $pinMensaje = false;
 
     if(empty($_POST['ip-inicial'])){
         $errorIpInicial = "Por favor introduce una IP inicial";
@@ -44,11 +47,28 @@
         $eliminar = true;
     }
 
+
+    if($buscar == true || $eliminar == true){
+        $claveIntroducido = $_POST['clave'];
+        if($claveIntroducido != $clave){
+            $buscar = false;
+            $eliminar = false;
+            $pinMensaje = true;
+            
+        } else {
+            $pinCorrecto = true;
+        }
+        
+    }
+
+
     function buscar_rango($inicio, $fin) {
         $inicio = ip2long($inicio);
         $fin = ip2long($fin);
         return array_map('long2ip', range($inicio, $fin) );
     }
+
+
 
 
 ?>
@@ -77,6 +97,7 @@
     </header>
 
     <h2><?php if(isset($errorIpInicial)) echo $errorIpInicial; ?></h2>
+    <h2><?php if($pinMensaje == true) echo 'PIN Incorrecto' ?></h2>
 
     <form action="index.php" class="formulario" method="post">
         <div class="contenedor-campos">
@@ -95,6 +116,12 @@
                 <label for="">Direccion final (opcional)</label>
                 <input class="input-text" type="text" placeholder="0.0.0.0" name="ip-final">
             </div>
+
+            <div class="campos">
+                <label for="">PIN</label>
+                <input class="input-text" type="password" name="clave">
+            </div>
+
             <div class="">
                 <input class="boton " type="submit" value="Buscar" name="buscar">
             </div>
@@ -113,8 +140,9 @@
 </body>
 </html>
 <?php 
-    if($buscar == true){
     
+    if($buscar == true && $pinCorrecto == true){
+
         // ping multi ip address
         $iplista = [$ipInicial,$ipFinal];
     
@@ -220,10 +248,7 @@
     }
 
 
-
-
-
-    if($eliminar == true){
+    if($eliminar == true && $pinCorrecto == true){
         $iplista = [$ipInicial,$ipFinal];
         $iplista = buscar_rango($iplista[0],$iplista[1]);
         $totalips = count($iplista);
@@ -235,6 +260,54 @@
             if($conn->query($sql) == TRUE){}
         }
 
+        $sql = "SELECT ip FROM direcciones";
+        $result = $conn->query($sql);
+
+        $todasIps = [];
+        if($result->num_rows>0){
+            while($row = $result->fetch_assoc()){
+                $todasIps[] = $row['ip'];
+            }
+        }
+
+        $totalips = count($todasIps);
+        $results = [];
+        for($i=0; $i<$totalips;$i++){
+            $ip = $todasIps[$i];
+            $ping = exec("ping -n 1 $ip",$salida,$estado);
+            $results[] = $estado;
+        }
+
+        // Table
+        echo '<font face=Lucida Console>';
+        echo "<table class='contenedor table' border=1 style=border-collapse:collapse>
+        <th colspan=4> Ping Monitoring </th>
+        <tr>
+            <td width=30></td>
+            <td width=250>IP</td>
+            <td width=220>Estado</td>
+        </tr>
+        ";
+        foreach($results as $item => $k){
+            echo "<tr>";
+            echo "<td class='text-decoration'>".$item."</td>";
+            echo "<td class='text-decoration'>".$todasIps[$item]."</td>";
+            if($results[$item]==0){
+                echo "<td class='text-decoration' style=color:green>Conectado</td>";
+            } else {
+                echo "<td class='text-decoration' style=color:grey>Desconectado</td>";
+            }
+            echo "</tr>";
+        }
+    
+        echo "</table>";
+        echo "</font>";
+    }
+
+
+
+
+    if($pinCorrecto == false){
         $sql = "SELECT ip FROM direcciones";
         $result = $conn->query($sql);
 
